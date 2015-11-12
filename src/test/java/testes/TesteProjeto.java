f10db1d04266c2cea43e44a5334f2329e7bba00f
Brendo <brendo10x@gmail.com>
2015-11-12 00:00:05 -0300

    Primeiro commit!

---
+++ src/test/java/testes/TesteProjeto.java
@@ -0,0 +1,26 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteProjeto {
+
+	@Test
+	public void teste1() {
+
+		assertTrue(true);
+	}
+
+	@Test
+	public void teste2() {
+
+		assertTrue(true);
+	}
+
+	@Test
+	public void teste3() {
+
+		assertTrue(true);
+	}
+}
